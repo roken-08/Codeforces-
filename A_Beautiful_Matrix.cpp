ddac46e503889a5a0d343aa8d23997ba00a84f23
roken <avngravngr@gmail.com>
2026-01-15 12:37:16 +0530

    Add files via upload

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int mat[5][5];
+    int row = 0;
+    int col = 0;
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            cin >> mat[i][j];
+            if(mat[i][j] == 1){
+                row = i;
+                col = j;
+            }
+        }
+    }
+    int ans = abs(row-2) + abs(col-2);
+    cout << ans;
+
+
+}
