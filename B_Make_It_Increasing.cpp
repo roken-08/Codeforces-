467bbc53a4b6a83b7b91d4b75cf8f7f56392a534
roken <avngravngr@gmail.com>
2026-02-04 22:29:42 +0530

    Add files via upload

---
+++ B_Make_It_Increasing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        int ans = 0;
+        for(int i = n-2; i >= 0; i--){
+            while(a[i] >= a[i+1]){
+                a[i]/=2;
+                ans++;
+                if(a[i] == 0) break;
+            }
+            if(a[i] == 0 && a[i+1] == 0){
+                ans = -1;
+                break;
+            }
+        }
+        
+        cout << ans << endl;
+    }
+}
